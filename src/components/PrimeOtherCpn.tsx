ba4619b5f86725b85d54d5d9fd63a3ecc036274d
ductc286 <ductc286@gmail.com>
2023-10-26 01:46:37 +0700

    no message

--- src/components/PrimeOtherCpn.tsx
+++ src/components/PrimeOtherCpn.tsx
@@ -1,73 +1,30 @@
-import React, { useState } from "react";
-import {
-  InputNumber,
-  InputNumberValueChangeEvent,
-} from "primereact/inputnumber";
+import React, { useRef } from "react";
+import { OverlayPanel } from "primereact/overlaypanel";
+import { Button } from "primereact/button";
 
-export default function NumeralsDemo() {
-  const [value1, setValue1] = useState<number>(42723);
-  const [value2, setValue2] = useState<number>(58151);
-  const [value3, setValue3] = useState<number>(2351.35);
-  const [value4, setValue4] = useState<number>(50);
+export default function BasicDemo() {
+  const op = useRef<OverlayPanel>(null);
 
   return (
-    <div className="card flex flex-wrap gap-3 p-fluid">
-      <div className="" style={{ width: "150px" }}>
-        <div className="flex-auto">
-          <label htmlFor="integeronly" className="font-bold block mb-2">
-            Integer Only
-          </label>
-          <InputNumber
-            inputId="integeronly"
-            value={value1}
-            onValueChange={(e: InputNumberValueChangeEvent) =>
-              setValue1(e.value ?? 0)
+    <>
+    <div style={{ minHeight: "500px" }}>ahi1</div>
+      <div className="card flex justify-content-center">
+        <Button
+          type="button"
+          icon="pi pi-image"
+          label="Image"
+          onClick={(e) => op.current?.toggle(e)}
+        />
+        <OverlayPanel ref={op}>
+          <img
+            src={
+              "https://primefaces.org/cdn/primereact/images/product/bamboo-watch.jpg"
             }
-          />
-        </div>
+            alt="Bamboo Watch"
+          ></img>
+        </OverlayPanel>
       </div>
-
-      <div className="flex-auto">
-        <label htmlFor="withoutgrouping" className="font-bold block mb-2">
-          Without Grouping
-        </label>
-        <InputNumber
-          inputId="withoutgrouping"
-          value={value2}
-          onValueChange={(e: InputNumberValueChangeEvent) =>
-            setValue2(e.value ?? 0)
-          }
-          useGrouping={false}
-        />
-      </div>
-      <div className="flex-auto">
-        <label htmlFor="minmaxfraction" className="font-bold block mb-2">
-          Min-Max Fraction Digits
-        </label>
-        <InputNumber
-          inputId="minmaxfraction"
-          value={value3}
-          onValueChange={(e: InputNumberValueChangeEvent) =>
-            setValue3(e.value ?? 0)
-          }
-          minFractionDigits={2}
-          maxFractionDigits={5}
-        />
-      </div>
-      <div className="flex-auto">
-        <label htmlFor="minmax" className="font-bold block mb-2">
-          Min-Max Boundaries
-        </label>
-        <InputNumber
-          inputId="minmax"
-          value={value4}
-          onValueChange={(e: InputNumberValueChangeEvent) =>
-            setValue4(e.value ?? 0)
-          }
-          min={0}
-          max={100}
-        />
-      </div>
-    </div>
+      <div style={{ minHeight: "500px" }}>ahi</div>
+    </>
   );
 }
